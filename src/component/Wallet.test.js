1d7b9826df322d8fe14b2d1ac81f03ad120b8d64
harold.lockward <harold.lockward@wirecard.com>
2019-09-10 15:53:39 -0400

    now it can make deposit

--- src/component/Wallet.test.js
+++ src/component/Wallet.test.js
@@ -3,7 +3,8 @@ import { shallow } from 'enzyme';
 import { Wallet } from './Wallet';
 
 describe('Wallet', () =>{
-    const props = {balance : 0};
+    const mockDeposit = jest.fn();
+    const props = {balance : 0, deposit : mockDeposit};
     const wallet = shallow(<Wallet {...props}/>);
     it('should render correctly', () =>{
         expect(wallet).toMatchSnapshot();
@@ -12,5 +13,30 @@ describe('Wallet', () =>{
     it('displays the balance from store', () =>{
         expect(wallet.find('.balance').text()).toEqual('Wallet balance: 0');
     });
+
+    it('create an input to deposit into or withdraw from the balance', () =>{
+        expect(wallet.find('.input-wallet').exists()).toBe(true);
+    });
+
+    describe('when the user types into the wallet input', () =>{
+        const userBalance = '25';
+
+        beforeEach(() =>{
+            wallet.find('.input-wallet').simulate('change', {target: {value : userBalance}});
+        });
+        it('update the local wallet balance in `state` and converts it to a number', () =>{
+            expect(wallet.state().balance).toEqual(parseInt(userBalance, 10));
+        });
+
+        describe('and the user wants to make a deposit', () =>{
+            beforeEach(() => wallet.find('.btn-deposit').simulate('click'));
+
+            it('dispatches the `deposit()` it receives from props with local balance', () =>{
+                expect(mockDeposit).toHaveBeenCalledWith(parseInt(userBalance,10));
+            });
+        });
+    });
+
+    
 
 });
