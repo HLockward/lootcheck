09de91f3d5f954c7b7d8f4b7715509552f2fb0dd
harold.lockward <harold.lockward@wirecard.com>
2019-09-19 16:08:55 -0400

    adding the Loot componet to app

--- src/component/Loot.test.js
+++ src/component/Loot.test.js
@@ -4,7 +4,7 @@ import { shallow, mount } from 'enzyme';
 
 describe('Loot', () =>{
     const mockFetchBitcoin = jest.fn();
-    let props = {balance: 10, bitcoin: {}, fetchBitcoin: mockFetchBitcoin};
+    var props = {balance: 10, bitcoin: {}, fetchBitcoin: mockFetchBitcoin};
     let loot = shallow(<Loot {...props} />);
 
     it('renders properly', () =>{
@@ -18,6 +18,19 @@ describe('Loot', () =>{
 
         it('dispatches the `fetchBitcoin()` method it receives from props', () => {
             expect(mockFetchBitcoin).toHaveBeenCalled();
+        });
+    });
+
+    describe('when there are valid bitcoin props', () =>{
+        beforeEach(() =>{
+            props = { balance: 10, bitcoin: { bpi: { USD: { rate: '1,000' }}}};
+            loot = shallow(<Loot  {...props}/>);
+            
+        });
+
+        it('displays the correct bitcoin value', () =>{
+            loot.debug();
+            expect(loot.find('h3').text()).toEqual('Bitcoin balance: 0.01');
         });
     });
 
--- src/index.js
+++ src/index.js
@@ -2,11 +2,12 @@ import React from 'react';
 import { render } from 'react-dom';
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
-import rootReducer from './redux/reducers/balance';
+import rootReducer from './redux/reducers';
 import { Provider } from 'react-redux';
+import logger from 'redux-logger';
 import App from './component/App';
 
-const store = createStore(rootReducer, applyMiddleware(thunk));
+const store = createStore(rootReducer, applyMiddleware(logger, thunk));
 
 render(
     <Provider store={store}>
